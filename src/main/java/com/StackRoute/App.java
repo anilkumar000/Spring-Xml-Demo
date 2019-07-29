32fe1720f15fc032cc42c17f8301fa8a09dacf1a
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-29 10:04:28 +0530

    Task2 Completed

--- src/main/java/com/StackRoute/App.java
+++ src/main/java/com/StackRoute/App.java
@@ -2,22 +2,21 @@ package com.StackRoute;
 
 
 import com.StackRoute.Task1.Movie;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 
 public class App 
 {
     public static void main( String[] args )
     {
-        XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
-        Movie movie1= (Movie)xmlBeanFactory.getBean("movie1");
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2=(Movie)applicationContext.getBean("movie2");
+        Movie movie1=(Movie)applicationContext.getBean("movie1");
         System.out.println(movie1.getActor());
-        System.out.println(movie2.getActor());
+
+        Movie movie3=(Movie)applicationContext.getBean("movie3");
+        System.out.println(movie3.getActor());
+        System.out.println(movie1==movie3);
 
 
     }
