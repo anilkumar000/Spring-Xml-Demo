c42f03ff13e3a015539bda235622fe16ebe3574b
venakatasatya.tutika <anilkumartutika000@gmail.com>
2019-07-29 22:04:06 +0530

    Task5 Completed

--- src/main/java/com/StackRoute/BeanLifeCycleDemo.java
+++ src/main/java/com/StackRoute/BeanLifeCycleDemo.java
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
-    public  static  void main(String args []) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
-        BeanLifeCycleDemo beanLifeCycleDemo = (BeanLifeCycleDemo) applicationContext.getBean("lifeCycle");
-    }
+
     public void Init ()
     {
         System.out.println("Initialization");
